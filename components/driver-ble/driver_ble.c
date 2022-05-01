2ffcfb262d18ef2524277f690a8eaab03d9849e0
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-05-01 10:53:11 -0500

    Configuración e inicialización de BLE

--- components/driver-ble/driver_ble.c
+++ components/driver-ble/driver_ble.c
@@ -1,7 +1,271 @@
-#include <stdio.h>
 #include "driver_ble.h"
 
-void func(void)
+void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param)
 {
+    // Si el tipo de evento es ESP_GATTS_REG_EVT, almacenar el gatts_if de cada perfil.
+    if (event == ESP_GATTS_REG_EVT) 
+    {
+        if (param->reg.status == ESP_GATT_OK) 
+        {
+            tab_perfil_hidratacion[IDX_APP_PERFIL].gatts_if = gatts_if;
 
+        } else 
+        {
+            ESP_LOGE(DRIVER_BLE_TAG, "No se pudo registrar el perfil, app_id %04x, status %d",
+                    param->reg.app_id,
+                    param->reg.status);
+            return;
+        }
+    }
+
+    do {
+        for (int i = 0; i < NUM_PERFILES; i++) 
+        {
+            if (gatts_if == ESP_GATT_IF_NONE || gatts_if == tab_perfil_hidratacion[i].gatts_if) 
+            {
+                if (tab_perfil_hidratacion[i].gatts_cb) 
+                {
+                    tab_perfil_hidratacion[i].gatts_cb(event, gatts_if, param);
+                }
+            }
+        }
+    } while (0);
+}
+
+void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param)
+{
+    switch (event)
+    {
+        case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+
+            config_adv_lista &= (~ADV_CONFIG_FLAG);
+
+            if (config_adv_lista == 0) {
+                esp_ble_gap_start_advertising(&params_descubrimiento);
+            }
+
+            break;
+        case ESP_GAP_BLE_SCAN_RSP_DATA_SET_COMPLETE_EVT:
+
+            config_adv_lista &= (~SCAN_RSP_CONFIG_FLAG);
+
+            if (config_adv_lista == 0) {
+                esp_ble_gap_start_advertising(&params_descubrimiento);
+            }
+
+            break;
+        case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+            // Revisar si el advertising comenzó con éxito.
+            if (param->adv_start_cmpl.status != ESP_BT_STATUS_SUCCESS)
+            {
+                ESP_LOGE(DRIVER_BLE_TAG, "No se pudo comenzar el advertising");
+            } else 
+            {
+                ESP_LOGI(DRIVER_BLE_TAG, "Advertising comenzado");
+            }
+            break;
+        case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+            // Revisar si el advertising fue detenido exitosamente.
+            if (param->adv_stop_cmpl.status != ESP_BT_STATUS_SUCCESS)
+            {
+                ESP_LOGE(DRIVER_BLE_TAG, "Error deteniendo el advertising");
+            } else 
+            {
+                ESP_LOGI(DRIVER_BLE_TAG, "Advertising detenido");
+            }
+            break;
+        case ESP_GAP_BLE_UPDATE_CONN_PARAMS_EVT:
+            ESP_LOGI(
+                DRIVER_BLE_TAG, 
+                "Parámetros de conexión actualizados:\nstatus = %d\nmin_int = %d\nmax_int = %d\nconn_int = %d\nlatency = %d\ntimeout = %d",
+                param->update_conn_params.status,
+                param->update_conn_params.min_int,
+                param->update_conn_params.max_int,
+                param->update_conn_params.conn_int,
+                param->update_conn_params.latency,
+                param->update_conn_params.timeout
+            );
+            break;
+        
+        default:
+            break;
+    }
+}
+
+esp_err_t init_driver_ble(const char* nombre, uint16_t uuid_app)
+{
+    esp_err_t status = ESP_OK;
+    esp_bt_controller_config_t config = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+
+    strncpy(nombre_dispositivo, nombre, 16);
+    nombre_dispositivo[16 - 1] = '\0';
+
+    status = esp_bt_controller_init(&config);
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "Error en la inicialización del controlador con configuración por defecto.");
+        return status;
+    }
+
+    status = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "El controlador no pudo ser activado");
+        return status;
+    }
+
+    status = esp_bluedroid_init();
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "La inicialización de Bluedroid falló.");
+        return status;
+    }
+
+    status = esp_bluedroid_enable();
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "La activación de Bluedroid falló.");
+        return status;
+    }
+
+    status = esp_ble_gatts_register_callback(gatts_event_handler);
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "No se pudo registrar el receptor de calbacks GATT: %x", status);
+        return status;
+    }
+
+    status = esp_ble_gap_register_callback(gap_event_handler);
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "No se pudo registrar el receptor de calbacks GAP, err: %x", status);
+        return status;
+    }
+
+    status = esp_ble_gatts_app_register(uuid_app);
+
+    if (status != ESP_OK)
+    {
+        ESP_LOGE(DRIVER_BLE_TAG, "No se pudo registrar la app, err: %x", status);
+        return status;
+    }
+
+    esp_err_t local_mtu_result = esp_ble_gatt_set_local_mtu(500);
+    if (local_mtu_result){
+        ESP_LOGE(DRIVER_BLE_TAG, "No se pudo configurar el nivel MTU local, err: %x", local_mtu_result);
+    }
+
+    return status;
+}
+
+void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param)
+{
+    switch (event) {
+        case ESP_GATTS_REG_EVT:
+        {
+            esp_err_t adv_status = ESP_OK;
+            adv_status = esp_ble_gap_set_device_name(nombre_dispositivo);
+            
+            if (ESP_OK != adv_status) {
+                ESP_LOGE(DRIVER_BLE_TAG, "No se pudo configurar el nombre del dispositivo, err: %x", adv_status);
+            }
+
+            // Config adv data.
+            adv_status = esp_ble_gap_config_adv_data(&datos_descubrimiento);
+            if (adv_status){
+                ESP_LOGE(DRIVER_BLE_TAG, "Fallo en configuración de adv, err: %x", adv_status);
+            }
+
+            config_adv_lista |= ADV_CONFIG_FLAG;
+
+            // Config scan response data
+            adv_status = esp_ble_gap_config_adv_data(&datos_resp_scan);
+            if (adv_status){
+                ESP_LOGE(DRIVER_BLE_TAG, "Fallo en configuración de respuesta a scan, err: %x", adv_status);
+            }
+
+            config_adv_lista |= SCAN_RSP_CONFIG_FLAG;
+
+            // Create the attribute table
+            esp_err_t create_attr_status = esp_ble_gatts_create_attr_tab(gatt_db, gatts_if, HIDR_IDX_NB + BAT_IDX_NB -1, ID_INST_SVC);
+            if (create_attr_status){
+                ESP_LOGE(DRIVER_BLE_TAG, "No se pudo crear la tabla de atributos, err: %x", create_attr_status);
+            }
+        }
+       	    break;
+               
+        case ESP_GATTS_READ_EVT:
+            ESP_LOGI(DRIVER_BLE_TAG, "ESP_GATTS_READ_EVT");
+       	    break;
+
+        case ESP_GATTS_MTU_EVT:
+            ESP_LOGI(DRIVER_BLE_TAG, "ESP_GATTS_MTU_EVT, MTU %d", param->mtu.mtu);
+            break;
+
+        case ESP_GATTS_CONF_EVT:
+            ESP_LOGI(DRIVER_BLE_TAG, "ESP_GATTS_CONF_EVT, status = %d, attr_handle %d", param->conf.status, param->conf.handle);
+            break;
+
+        case ESP_GATTS_START_EVT:
+            ESP_LOGI(DRIVER_BLE_TAG, "SERVICE_START_EVT, status %d, service_handle %d", param->start.status, param->start.service_handle);
+            break;
+
+        case ESP_GATTS_CONNECT_EVT:
+            ESP_LOGI(DRIVER_BLE_TAG, "ESP_GATTS_CONNECT_EVT, conn_id = %d", param->connect.conn_id);
+            esp_log_buffer_hex(DRIVER_BLE_TAG, param->connect.remote_bda, 6);
+            esp_ble_conn_update_params_t conn_params = {0};
+            memcpy(conn_params.bda, param->connect.remote_bda, sizeof(esp_bd_addr_t));
+
+            conn_params.latency = 0;
+            conn_params.max_int = 0x20;    // max_int = 0x20*1.25ms = 40ms
+            conn_params.min_int = 0x10;    // min_int = 0x10*1.25ms = 20ms
+            conn_params.timeout = 400;    // timeout = 400*10ms = 4000ms
+
+            //Enviar los parámetros de conección actualizados al dispositivo emparejado.
+            esp_ble_gap_update_conn_params(&conn_params);
+            break;
+
+        case ESP_GATTS_DISCONNECT_EVT:
+            ESP_LOGI(DRIVER_BLE_TAG, "ESP_GATTS_DISCONNECT_EVT, reason = 0x%x", param->disconnect.reason);
+            esp_ble_gap_start_advertising(&params_descubrimiento);
+            break;
+
+        case ESP_GATTS_CREAT_ATTR_TAB_EVT:
+        {
+            if (param->add_attr_tab.status != ESP_GATT_OK)
+            {
+                ESP_LOGE(DRIVER_BLE_TAG, "Fallo en creación de tabla de atributos, err: 0x%x", param->add_attr_tab.status);
+            }
+            else if (param->add_attr_tab.num_handle != HIDR_IDX_NB + BAT_IDX_NB -1){
+                ESP_LOGE(DRIVER_BLE_TAG, "create attribute table abnormally, num_handle (%d) \
+                        no es igual a la HIDR_IDX_NB + BAT_IDX_NB -1(%d)", param->add_attr_tab.num_handle, HIDR_IDX_NB + BAT_IDX_NB -1);
+            }
+            else {
+                ESP_LOGI(DRIVER_BLE_TAG, "Tabla de atributos creada, handle = %d\n",param->add_attr_tab.num_handle);
+                memcpy(tabla_handles, param->add_attr_tab.handles, sizeof(tabla_handles));
+
+                esp_ble_gatts_start_service(tabla_handles[IDX_SVC_HIDRATACION]);
+                esp_ble_gatts_start_service(tabla_handles[IDX_SVC_BATERIA]);
+            }
+            break;
+        }
+        case ESP_GATTS_STOP_EVT:
+        case ESP_GATTS_OPEN_EVT:
+        case ESP_GATTS_CANCEL_OPEN_EVT:
+        case ESP_GATTS_CLOSE_EVT:
+        case ESP_GATTS_LISTEN_EVT:
+        case ESP_GATTS_CONGEST_EVT:
+        case ESP_GATTS_UNREG_EVT:
+        case ESP_GATTS_DELETE_EVT:
+        case ESP_GATTS_WRITE_EVT: // Not sure about these last two
+        case ESP_GATTS_EXEC_WRITE_EVT:
+        default:
+            break;
+    }
 }
--- components/driver-ble/include/driver_ble.h
+++ components/driver-ble/include/driver_ble.h
@@ -1,1 +1,243 @@
-void func(void);
+#ifndef _DRIVER_BLE_H_
+#define _DRIVER_BLE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <esp_bt.h> 
+#include <esp_gap_ble_api.h>
+#include <esp_gatts_api.h>
+#include <esp_bt_main.h>
+#include <esp_gatt_common_api.h>
+
+#include <esp_system.h> 
+#include <esp_log.h> 
+
+#define DRIVER_BLE_TAG "DRIVER_BLE"
+
+#define NUM_PERFILES            1
+#define IDX_APP_PERFIL          0
+#define ID_INST_SVC             0
+#define LONGITUD_MAX_CHAR_GATTS 500
+#define SIZE_DECLARACION_CHAR (sizeof(uint8_t))
+
+#define ADV_CONFIG_FLAG             (1 << 0)
+#define SCAN_RSP_CONFIG_FLAG        (1 << 1)
+
+/* Atributos GATT del servicio de hidratacion perfil. */
+enum {
+    IDX_SVC_HIDRATACION,    /* Índice del servicio de hidratación. */
+
+    HIDR_IDX_CHAR_MILILITROS, /* Índice de la característica de cantidad de agua en ml. */
+    HIDR_IDX_VAL_MILILITROS,  /* Índice del valor de la característica de cantidad de agua en ml. */
+
+    HIDR_IDX_CHAR_TEMP,     /* Índice de la característica de temperatura aproximada. */
+    HIDR_IDX_VAL_TEMP,      /* Índice del valor de la característica de temperatura aprox. en C. */
+
+    HIDR_IDX_CHAR_FECHA,    /* Índice del valor de la característica de fecha (timestamp). */
+    HIDR_IDX_VAL_FECHA,     
+
+    HIDR_IDX_CHAR_REGISTROS_NUEVOS,
+    HIDR_IDX_VAL_REGISTROS_NUEVOS,
+    HIDR_IDX_CHAR_REGISTROS_NUEVOS_NTF_CFG,
+
+    HIDR_IDX_NB
+};
+
+enum {
+    IDX_SVC_BATERIA,        /* Índice del servicio de batería. */
+
+    BAT_IDX_CHAR_NIVEL,
+    BAT_IDX_VAL_NIVEL,
+    BAT_IDX_CHAR_NIVEL_NTF_CFG,
+
+    BAT_IDX_NB,
+};
+
+static uint8_t config_adv_lista = 0;
+
+static char nombre_dispositivo[16] = "Hydrate-0000";
+
+uint16_t tabla_handles[HIDR_IDX_NB + BAT_IDX_NB -1];
+
+static uint8_t uuid_servicio_hidr[16] = {
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x11, 0xF0, 0xD7, 0x00, 0x00,
+};
+
+static uint8_t uuid_servicio_bat[16] = {
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x0f, 0x18, 0x00, 0x00,
+};
+
+static const uint16_t GATTS_SERV_UUID_BATERIA       = 0x180F;
+static const uint32_t GATTS_CHAR_UUID_CANT_ML       = 0x0faf892c;
+static const uint16_t GATTS_CHAR_UUID_TEMP_AMB      = 0x2a6e;
+static const uint16_t GATTS_CHAR_UUID_FECHA         = 0x0fff;
+static const uint32_t GATTS_CHAR_UUID_NUEVOS_REG    = 0x0faf892f;
+static const uint16_t GATTS_CHAR_UUID_BATERIA       = 0x2a19;
+
+static esp_ble_adv_params_t params_descubrimiento = {
+    .adv_int_min = 0x20,
+    .adv_int_max = 0x40,
+    .adv_type = ADV_TYPE_IND,
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
+static esp_ble_adv_data_t datos_descubrimiento = {
+    .set_scan_rsp        = false,
+    .include_name        = true,
+    .include_txpower     = true,
+    .min_interval        = 0x0006,
+    .max_interval        = 0x0010,
+    .appearance          = 0x00,
+    .manufacturer_len    = 0, 
+    .p_manufacturer_data = NULL,
+    .service_data_len    = 0,
+    .p_service_data      = NULL,
+    .service_uuid_len    = sizeof(uuid_servicio_hidr),
+    .p_service_uuid      = uuid_servicio_hidr,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+static esp_ble_adv_data_t datos_resp_scan = {
+    .set_scan_rsp        = true,
+    .include_name        = true,
+    .include_txpower     = true,
+    .min_interval        = 0x0006,
+    .max_interval        = 0x0010,
+    .appearance          = 0x00,
+    .manufacturer_len    = 0, 
+    .p_manufacturer_data = NULL,
+    .service_data_len    = 0,
+    .p_service_data      = NULL,
+    .service_uuid_len    = sizeof(uuid_servicio_hidr),
+    .p_service_uuid      = uuid_servicio_hidr,
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+};
+
+/* Service */
+static const uint16_t GATTS_SERVICE_UUID_WS         = 0x181a;
+static const uint16_t GATTS_CHAR_UUID_AMB_TEMP      = 0x2a6e;
+static const uint16_t GATTS_CHAR_UUID_AMB_HUM       = 0x2a6f;
+// static const uint16_t GATTS_CHAR_UUID_HEAT_INDEX    = 0x2a7a;
+
+static const uint16_t uuid_servicio_primario       = ESP_GATT_UUID_PRI_SERVICE;
+static const uint16_t uuid_servicio_secundario     = ESP_GATT_UUID_SEC_SERVICE;
+static const uint16_t uuid_declaracion_char        = ESP_GATT_UUID_CHAR_DECLARE;
+static const uint16_t uuid_config_cliente_char     = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
+static const uint8_t propiedad_char_leer           = ESP_GATT_CHAR_PROP_BIT_READ;
+static const uint8_t propiedad_char_escribir       = ESP_GATT_CHAR_PROP_BIT_WRITE;
+static const uint8_t propiedad_char_leer_escribir_notif = ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
+static const uint8_t ccc_char_idx_nuevos_reg[2]    = {0x00, 0x00};
+static const uint8_t ccc_char_idx_nivel_bat[2]    = {0x00, 0x00};
+//TODO: Cambiar esto
+static const uint8_t valor_char[4]                 = {0x11, 0x22, 0x33, 0x44};
+
+struct inst_perfil_gatts {
+    esp_gatts_cb_t gatts_cb;
+    uint16_t gatts_if;
+    uint16_t app_id;
+    uint16_t conn_id;
+    uint16_t service_handle;
+    esp_gatt_srvc_id_t service_id;
+    uint16_t char_handle;
+    esp_bt_uuid_t char_uuid;
+    esp_gatt_perm_t perm;
+    esp_gatt_char_prop_t property;
+    uint16_t descr_handle;
+    esp_bt_uuid_t descr_uuid;
+};
+
+esp_err_t init_driver_ble(const char* nombre, uint16_t uuid_app);
+
+void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param);
+
+void gatts_profile_event_handler(esp_gatts_cb_event_t event,
+					esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+static struct inst_perfil_gatts tab_perfil_hidratacion[NUM_PERFILES] = {
+    [IDX_APP_PERFIL] = {
+        .gatts_cb = gatts_profile_event_handler,
+        .gatts_if = ESP_GATT_IF_NONE
+    },
+};
+
+/* Descripción completa de las características de los servicios del perfil BLE. */
+static const esp_gatts_attr_db_t gatt_db[HIDR_IDX_NB + BAT_IDX_NB] =
+{
+    // Declaracion del servicio de hidratacion.
+    [IDX_SVC_HIDRATACION] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_128, (uint8_t *)&uuid_servicio_primario, ESP_GATT_PERM_READ,
+      sizeof(uint16_t), sizeof(uuid_servicio_hidr), (uint8_t *)&uuid_servicio_hidr}},
+
+    /* Declaracion de la caracteristica de mililitros. */
+    [HIDR_IDX_CHAR_MILILITROS] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_declaracion_char, ESP_GATT_PERM_READ,
+      SIZE_DECLARACION_CHAR, SIZE_DECLARACION_CHAR, (uint8_t *)&propiedad_char_leer}},
+
+    /* Configurar el valor de la caracteristica de mililitros. */
+    [HIDR_IDX_VAL_MILILITROS] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_32, (uint8_t *)&GATTS_CHAR_UUID_CANT_ML, ESP_GATT_PERM_READ,
+      LONGITUD_MAX_CHAR_GATTS, sizeof(valor_char), (uint8_t *)valor_char}},
+    
+    /* Declaracion de la caracteristica de temperatura. */
+    [HIDR_IDX_CHAR_TEMP] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_declaracion_char, ESP_GATT_PERM_READ,
+      SIZE_DECLARACION_CHAR, SIZE_DECLARACION_CHAR, (uint8_t *)&propiedad_char_leer}},
+
+    /* Configurar el valor de la caracteristica de temperatura. */
+    [HIDR_IDX_VAL_TEMP] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_AMB_TEMP, ESP_GATT_PERM_READ,
+      LONGITUD_MAX_CHAR_GATTS, sizeof(valor_char), (uint8_t *)valor_char}},
+    
+    /* Declaracion de la caracteristica de fecha. */
+    [HIDR_IDX_CHAR_FECHA] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_declaracion_char, ESP_GATT_PERM_READ,
+      SIZE_DECLARACION_CHAR, SIZE_DECLARACION_CHAR, (uint8_t *)&propiedad_char_leer}},
+
+    /* Configurar el valor de la caracteristica de fecha. */
+    [HIDR_IDX_VAL_FECHA] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_FECHA, ESP_GATT_PERM_READ,
+      LONGITUD_MAX_CHAR_GATTS, sizeof(valor_char), (uint8_t *)valor_char}},
+    
+    /* Declaracion de la caracteristica de registros nuevos. */
+    [HIDR_IDX_CHAR_REGISTROS_NUEVOS] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_declaracion_char, ESP_GATT_PERM_READ,
+      SIZE_DECLARACION_CHAR, SIZE_DECLARACION_CHAR, (uint8_t *)&propiedad_char_leer}},
+
+    /* Configurar el valor de la caracteristica de registros nuevos. */
+    [HIDR_IDX_VAL_REGISTROS_NUEVOS] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_32, (uint8_t *)&GATTS_CHAR_UUID_NUEVOS_REG, ESP_GATT_PERM_READ,
+      LONGITUD_MAX_CHAR_GATTS, sizeof(valor_char), (uint8_t *)valor_char}},
+
+    // Client Characteristic Configuration Descriptor (CCCD) de registros nuevos.
+    [HIDR_IDX_CHAR_REGISTROS_NUEVOS_NTF_CFG] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_config_cliente_char, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+      sizeof(uint16_t), sizeof(ccc_char_idx_nuevos_reg), (uint8_t *)ccc_char_idx_nuevos_reg}},
+
+    // Declaracion del servicio de bateria.
+    [IDX_SVC_BATERIA] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_servicio_secundario, ESP_GATT_PERM_READ,
+      sizeof(uint16_t), sizeof(GATTS_SERV_UUID_BATERIA), (uint8_t *)&GATTS_SERV_UUID_BATERIA}},
+
+    /* Characteristic Declaration */
+    [BAT_IDX_CHAR_NIVEL] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_declaracion_char, ESP_GATT_PERM_READ,
+      SIZE_DECLARACION_CHAR, SIZE_DECLARACION_CHAR, (uint8_t *)&propiedad_char_leer}},
+
+    /* Characteristic Value */
+    [BAT_IDX_VAL_NIVEL] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_BATERIA, ESP_GATT_PERM_READ,
+      LONGITUD_MAX_CHAR_GATTS, sizeof(valor_char), (uint8_t *)valor_char}},
+    
+    // Client Characteristic Configuration Descriptor (CCCD) de registros nuevos.
+    [BAT_IDX_CHAR_NIVEL_NTF_CFG] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&uuid_config_cliente_char, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+      sizeof(uint16_t), sizeof(ccc_char_idx_nivel_bat), (uint8_t *)ccc_char_idx_nivel_bat}},
+};
+
+#endif /* _DRIVER_BLE_H_ */
