d4cd76bcacfd65628656834866e3e2ca330f032d
FrozenBurrito <f3rm3ndouza@outlook.com>
2022-09-20 13:00:59 -0500

    agregar componente con sensor de bateria

---
+++ components/battery_monitor/include/battery_monitor.h
@@ -0,0 +1,22 @@
+#ifndef _BATTERY_MONITOR_H_
+#define _BATTERY_MONITOR_H_
+
+#include <stdbool.h>
+#include <esp_err.h>
+#include <driver/gpio.h>
+#include <driver/adc.h>
+#include <soc/adc_channel.h>
+#include <esp_adc_cal.h>
+
+typedef struct {
+    uint8_t remaining_charge;
+    int32_t aprox_ms_until_discharge;
+} battery_measurement_t;
+
+esp_err_t battery_monitor_init(void);
+
+esp_err_t get_battery_level(battery_measurement_t* out_bat_measurement);
+
+esp_err_t multi_sample_battery_level(battery_measurement_t* out_bat_measurement, size_t number_of_samples);
+
+#endif /** _BATTERY_MONITOR_H_ **/
